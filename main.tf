58a5b55bfc577d57b62691b48e54f18304fc3f25
anshul989 <anshulsaluja4@gmail.com>
2022-09-18 23:00:46 +0530

    full fledge

---
+++ main.tf
@@ -0,0 +1,15 @@
+
+resource "aws_instance" "web" {
+
+  ami     = var.ami[count.index]
+
+  instance_type = "t2.micro"
+
+  count = length(var.ami)
+
+  tags = {
+    Name = var.ins_name[count.index],
+    Owner = "anshul Kumar"
+
+  }
+}
